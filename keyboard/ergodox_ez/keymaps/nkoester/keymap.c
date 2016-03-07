9859a094292f7ab8ad226ba846573e291c2cbc99
Norman Köster <nkoester@techfak.uni-bielefeld.de>
2016-03-07 11:53:02 +0100

    Move tmp switch a row down && Add media key

--- keyboard/ergodox_ez/keymaps/nkoester/keymap.c
+++ keyboard/ergodox_ez/keymaps/nkoester/keymap.c
@@ -18,9 +18,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+-------+------+----------------|        |---------+------+------+------+------+------+-----------|
  * | Tab    |   Q  |   W  |   E   |   R  |   T  |  COPY   |        |  UNDO   |   Y  |   U  |   I  |   O  |   P  |   \ |     |
  * |--------+------+------+-------+------+------|         |        |         |------+------+------+------+------+-----------|
- * | LSHIFT |A(MDA)|   S  |   D   |   F  |   G  |---------|        |---------|   H  |   J  |   K  |   L  |;(MDA)|'" (RShift)|
+ * | LSHIFT |   A  |   S  |   D   |   F  |   G  |---------|        |---------|   H  |   J  |   K  |   L  |   ;  |'" (RShift)|
  * |--------+------+------+-------+------+------|  PASTE  |        | REDO    |------+------+------+------+------+-----------|
- * | CTRL   |   Z  |   X  |   C   |   V  |   B  |         |        |         |   N  |   M  |   ,  |   .  | /  ? |           |
+ * | CTRL   |Z(MDA)|   X  |   C   |   V  |   B  |         |        |         |   N  |   M  |   ,  |   .  |/?(MDA)|           |
  * `--------+------+------+-------+------+----------------'        `----------------+------+------+------+------+-----------'
  *   |  `~  |      |  [ { |  LGUI |  ALT |                                          | ALT  | LGUI |  ] } |      |  '#  |
  *   |(SYM) |      |      |       |      |                                          |      |      |      |      |(SYM) |
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |  Home | End  |       | PgUp |  PgDn  |
  *                                        |       |      |       |      |        |
  *                                 ,------|-------|------|       |------+--------+------.
- *                                 |      |       |      |       |      |        |      |
+ *                                 |      |       | App  |       |      |        |      |
  *                                 |Space |Backsp |------|       |------|  Del   |Enter |
  *                                 |      |ace    |Enter |       |      |        |      |
  *                                 `--------------------'       `----------------------'
@@ -40,19 +40,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,                KC_1,         KC_2,           KC_3,     KC_4,   KC_5,   M(MCUT),
         KC_TAB,                KC_Q,         KC_W,           KC_E,     KC_R,   KC_T,   M(MCOPY),
-        KC_LSFT,     LT(MDIA, KC_A),         KC_S,           KC_D,     KC_F,   KC_G,
-        KC_LCTRL,              KC_Z,         KC_X,           KC_C,     KC_V,   KC_B,   M(MPSTE),
+        KC_LSFT,               KC_A,         KC_S,           KC_D,     KC_F,   KC_G,
+        KC_LCTRL,    LT(MDIA, KC_Z),         KC_X,           KC_C,     KC_V,   KC_B,   M(MPSTE),
     LT(SYMB,KC_GRV),          KC_NO,      KC_LBRC,        KC_LGUI,  KC_LALT,
 
                                                           KC_HOME,       KC_END,
-                                                                          KC_NO,
+                                                                         KC_APP,
                                          KC_SPC,          KC_BSPC,       KC_ENT,
 
                                                                                                         // right hand
                                                                                   ACTION_MODS_KEY(MOD_LGUI, KC_ENT),       KC_6,      KC_7,     KC_8,     KC_9,           KC_0,                KC_MINS,
                                                                                                         LCTL(KC_Z),        KC_Y,      KC_U,     KC_I,     KC_O,           KC_P,                KC_BSLS,
-                                                                                                                           KC_H,      KC_J,     KC_K,     KC_L,           LT(MDIA, KC_SCLN),   SFT_T(KC_QUOT),
-                                                                                                        LCTL(KC_Y),        KC_N,      KC_M,     KC_COMM,  KC_DOT,         KC_SLSH,             KC_NO,
+                                                                                                                           KC_H,      KC_J,     KC_K,     KC_L,           KC_SCLN,             SFT_T(KC_QUOT),
+                                                                                                        LCTL(KC_Y),        KC_N,      KC_M,     KC_COMM,  KC_DOT,         LT(MDIA, KC_SLSH),   KC_NO,
                                                                                                                                    KC_LALT,     KC_LGUI,  KC_RBRC,        KC_NO,               LT(SYMB,KC_QUOT),
 
                                                                                                         KC_PGUP, KC_PGDN,
@@ -111,9 +111,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      | MsUp |      |      | MwUp |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      | Left | Up   | Down | Right  |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      | Left | Down |  Up  | Right| VolUp  |
  * |--------+------+------+------+------+------| MWdn |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      | VolDn| VolUp  |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      | VolDn  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      | Mute | Play | Prev | Next |
  *   `----------------------------------'                                       `----------------------------------'
@@ -140,8 +140,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                     // right hand
                                                                        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                                        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                                                 KC_TRNS, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_TRNS,
-                                                                       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU,
+                                                                                 KC_TRNS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_VOLU,
+                                                                       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
                                                                                           KC_TRNS, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT,
 
                                                                        KC_TRNS, KC_TRNS,
@@ -151,7 +151,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),               // FN1 - Momentary Layer 1 (Symbols)
+    [2] = ACTION_LAYER_TAP_TOGGLE(MDIA)                // FN2 - Momentary Layer 2 (Media)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
